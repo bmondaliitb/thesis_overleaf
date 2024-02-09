43098b7153e42e7949295f97dbbcc3b48f845844
Buddhadeb Mondal <buddha@Buddhadebs-MacBook-Pro.local>
2024-02-09 09:41:53 +0100

    added the background temaplate

---
+++ Figures/egammafakes/converted_ph/syst_sig_var/CopyFiles.sh
@@ -0,0 +1,2 @@
+cp ~/work/ttgamma-analysis/efake_calculation_18082021/efakeestimation/fitting_package/run/converted/syst_sig_var/Zee_fit_eta1pt1.pdf .
+cp ~/work/ttgamma-analysis/efake_calculation_18082021/efakeestimation/fitting_package/run/converted/syst_sig_var/Zeg_fit_eta1pt1.pdf .
